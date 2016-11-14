73a0f3cb82ffdd174c2ff1b000e2da5d5d86bfc1
Andrey Solovev Home <andreysoloweb@gmail.com>
2016-11-15 02:05:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ionic-quick-start
---
+++ src/pages/users/users.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { User } from '../../models/user';
+
+import {  GithubUsers } from '../../providers/github-users';
+
+/*
+  Generated class for the Users page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html'
+})
+export class UsersPage {
+  users: User[]
+
+  constructor(public navCtrl: NavController, private githubUsers: GithubUsers) {
+    githubUsers.load().subscribe(users => {
+      this.users = users;
+    })
+  }
+
+}
